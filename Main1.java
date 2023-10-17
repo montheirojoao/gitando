2ce1b067f9b265f7c039aa64293023f1021f9ea6
JOAO MONTEIRO <141248283+montheirojoao@users.noreply.github.com>
2023-10-17 08:34:29 -0300

    Create Main1.java

---
+++ Main1.java
@@ -0,0 +1,15 @@
+public class Main1{
+
+  public static void main (Strings[] args){
+         Scanner sc = new Scanner(System.in);
+
+        system.out.println("Olá Mundo")
+        system.out.println("Olá Mundo")
+        system.out.println("Olá Mundo")
+        system.out.println("Olá Mundo")
+        system.out.println("Olá Mundo")
+        system.out.println("Olá Mundo")
+        system.out.println("Olá Mundo")
+        system.out.println("Olá Mundo")
+}
+}
